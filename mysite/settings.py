b907ce353b0bae141f6b4eb09c917735652fb767
Jagruti1004 <jagrutibasa1004@gmail.com>
2021-07-14 17:20:16 +0530

    Added local_settings file and modified settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,3 +130,9 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from mysite.local_settings import *
+except ImportError:
+    pass
